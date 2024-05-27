a463818248a988949422799e546e45fc91934d34
Josuephyus <67875659+Josuephyus@users.noreply.github.com>
2024-05-27 02:34:58 -0700

    Added Concept to Shape
    Created the concept of shapes

--- util/Shape.java
+++ util/Shape.java
@@ -1,5 +1,7 @@
 package util;
 
 public class Shape {
-    
+    //OKAY, shapes are complicated, but because we are only going to use Squares and Triangles, what we can do is the line method
+    // Basically, we get the point. With the point, we make a line. The line goes all right. We check if that line intersects with one
+    // of the shapes lines. If it intersects with only one, it is inside, otherwise it is outside. Capish?
 }
