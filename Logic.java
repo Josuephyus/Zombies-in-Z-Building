b7443db17c5db2711dc1d6a4ed08fb9b8eb6ce56
Josuephyus <67875659+Josuephyus@users.noreply.github.com>
2024-05-28 15:36:05 -0700

    Squashed commit of the following:
    commit a94f342046c3bdea9f900daabb8c8bd66337adc0
    Author: Josuephyus <67875659+Josuephyus@users.noreply.github.com>
    Date:   Tue May 28 15:34:21 2024 -0700
    
        Update Logic.java

--- Logic.java
+++ Logic.java
@@ -17,7 +17,6 @@ public class Logic {
         if (Listener.Key.keyActive[1])pm.x--;
         if (Listener.Key.keyActive[2])pm.y--;
         if (Listener.Key.keyActive[3])pm.x++;
-
         if (p.distance(pm) != 0){
             Double theta = p.directionTo(pm);
             System.out.println(theta);
